f0a410dbc4e36e6388c6ba72abe90e01489610ff
Miguel Peixe <miguel@cardume.art.br>
2013-05-13 09:03:48 -0300

    add official location data

--- scraper.js
+++ scraper.js
@@ -1,11 +1,15 @@
 (function($) {
 
-	var full_page = 'projects.html';
+	var projects_url = 'data/projects.html';
+
+	var places_url = 'http://viradacultural.prefeitura.sp.gov.br/programacao?width=470&height=760&em_ajax=true&query=GlobalMapData&callback=?';
+
+	var places_fallback_url = 'data/places.json';
 
 	var projects = [];
 
 	$.ajax({
-		url: full_page,
+		url: projects_url,
 		dataType: 'text',
 		error: function(jqxhr, status, error) {
 			console.log(status + ' ' + error);
@@ -15,115 +19,136 @@
 
 	function scrap(data) {
 
-		var $page = $(data);
+		// get places
+		$.ajax({
+			url: places_url,
+			dataType: 'json',
+			error: function(jqxhr, status, error) {
+				console.log(status + ' ' + error);
+				$.ajax({
+					url: places_fallback_url,
+					dataType: 'json',
+					error: function() {
+						alert('Alguma coisa deu errado, desculpe');
+					},
+					success: doYourThing
+				})
+			},
+			success: doYourThing
+		})
 
-		$page.find('h2').each(function() {
+		function doYourThing(places) {
 
-			var place = $(this).text();
+			var $page = $(data);
 
-			var day, time;
+			$page.find('h2').each(function() {
 
-			var lat = -23.53;
+				var place = $(this).text();
 
-			var lng = -46.64;
+				var placeData = _.find(places, function(p) { return p.location_name.indexOf(place) !== -1; })
 
-			var $placeData = $(this).nextUntil('br');
+				var day, time;
 
-			$placeData.each(function(i) {
+				var $placeData = $(this).nextUntil('br');
 
-				if($(this).is('h2')) {
-					return false;
-				}
+				$placeData.each(function(i) {
 
-				if($(this).is('h3')) {
+					if($(this).is('h2')) {
+						return false;
+					}
 
-					day = trim($(this).text());
+					if($(this).is('h3')) {
 
-				} else if($(this).is('dt')) {
+						day = trim($(this).text());
 
-					time = trim($(this).text());
+					} else if($(this).is('dt')) {
 
-				} else {
+						time = trim($(this).text());
 
-					projects.push({
-						name: trim($(this).text()),
-						day: day,
-						time: time,
-						place: place,
-						lat: lat,
-						lng: lng
-					});
+					} else {
 
-					lat = lat - (i*0.001);
-					lng = lng - (i*0.001);
+						var project = {
+							name: trim($(this).text()),
+							day: day,
+							time: time,
+							place: place
+						}
 
-				}
+						_.extend(project, placeData);
+
+						projects.push(project);
+
+					}
+
+				});
 
 			});
 
-		});
-
-		var config = {
-			data: projects,
-			dataRef: {
-				lat: 'lat',
-				lng: 'lng'
-			},
-			map: {
-				center: [-23.5369, -46.6478],
-				zoom: 12,
-				maxZoom: 18,
-				markers: {
-					cluster: true
-				}
-			},
-			filters: [
-				{
-					name: 's',
-					sourceRef: 'name',
-					type: 'text',
-					title: 'Busca por nome'
+			var config = {
+				data: projects,
+				dataRef: {
+					lat: 'location_latitude',
+					lng: 'location_longitude'
 				},
-				{
-					name: 'dia',
-					sourceRef: 'day',
-					type: 'multiple-select',
-					title: 'Dias'
+				map: {
+					center: [-23.5369, -46.6478],
+					zoom: 11,
+					maxZoom: 18,
+					markers: {
+						cluster: true
+					}
 				},
-				{
-					name: 'local',
-					sourceRef: 'place',
-					type: 'multiple-select',
-					title: 'Local'
+				filters: [
+					{
+						name: 's',
+						sourceRef: 'name',
+						type: 'text',
+						title: 'Busca por nome'
+					},
+					{
+						name: 'dia',
+						sourceRef: 'day',
+						type: 'multiple-select',
+						title: 'Dias'
+					},
+					{
+						name: 'local',
+						sourceRef: 'place',
+						type: 'multiple-select',
+						title: 'Local'
+					},
+					{
+						name: 'horario',
+						sourceRef: 'time',
+						type: 'multiple-select',
+						title: 'Hora'
+					}
+				],
+				templates: {
+					marker: '<h4><%= item.name %></h4><%= item.location_balloon %>',
+					list: '<p class="category"><%= item.place %></p><h3><%= item.name %></h3><p><%= item.day %>, às <%= item.time %></p>',
+					single: '<p class="cat"><%= item.place %></p><h2><%= item.name %></h2><p><%= item.day %>, às <%= item.time %></p><p><%= item.post_content %></p>'
 				},
-				{
-					name: 'horario',
-					sourceRef: 'time',
-					type: 'multiple-select',
-					title: 'Hora'
-				}
-			],
-			templates: {
-				list: '<p class="category"><%= item.place %></p><h3><%= item.name %></h3><p><%= item.day %>, às <%= item.time %></p>',
-				single: '<p class="cat"><%= item.place %></p><h2><%= item.name %></h2><p><%= item.day %>, às <%= item.time %></p>'
-			},
-			labels: {
-				title: 'VIRADA CULTURAL 2013',
-				subtitle: '<strong>//Visualização hackeada</strong>',
-				filters: 'Filtros',
-				results: 'Resultados',
-				clear_search: 'Limpar busca',
-				close: 'Fechar',
-				view_map: 'Ver mapa',
-				loading: {
-					first: 'Carregando eventos...',
-					item: 'Carregando...',
-					error: 'Ops, parece que o servidor de dados está fora do ar. Tente novamente.'
+				labels: {
+					title: 'VIRADA CULTURAL 2013',
+					subtitle: '<strong>//Visualização hackeada. Original <a href="http://viradacultural.prefeitura.sp.gov.br/programacao" target="_blank" rel="external">aqui</a></strong>',
+					filters: 'Filtros',
+					results: 'Resultados',
+					clear_search: 'Limpar busca',
+					close: 'Fechar',
+					view_map: 'Ver mapa',
+					loading: {
+						first: 'Carregando eventos...',
+						item: 'Carregando...',
+						error: 'Ops, parece que o servidor de dados está fora do ar. Tente novamente.'
+					}
 				}
 			}
-		}
 
-		carttirail.init('app', config);
+
+			carttirail.init('app', config);
+
+		}
 
 	}
 
